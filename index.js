f3fc4863ff9a522180ac6df156abeb857fe7497a
marceloambarc <marcelo.ambarc@gmail.com>
2021-08-16 11:26:37 -0300

    16/08 - 11:26

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
+const express = require('express')
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const wbm = require('wbm');
 
-wbm.start().then(async res => {
-  console.log(res);
-  await wbm.waitQRCode();
-  for (let index = 0; index < 4; index++) {
-    const phones = ['xxxxxx00001111'];
-    const message = `Loop ${index}`;
-    await wbm.send(phones, message);
-  }
-  await wbm.end();
-}).catch(err => console.log(err));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/', function (req,res){
+  wbm.start().then(async wbmResponse => {
+    res.send(`<img src=${wbmResponse} alt="qrCode" />`);
+    await wbm.waitQRCode();
+    for (let index = 0; index < 4; index++) {
+      const phones = ['xxxxxx00001111'];
+      const message = `Loop ${index}`;
+      await wbm.send(phones, message);
+    }
+    await wbm.end();
+  }).catch(err => console.log(err));
+});
+
+app.listen(3000,() => {
+  console.log('Server On');
+});
+
+
 
