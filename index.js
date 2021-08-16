588d8dae74db6aef252bf82c24475225a8fb40e8
marceloambarc <marcelo.ambarc@gmail.com>
2021-08-16 17:24:04 -0300

    16/08 - 17:24

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 const wbm = require('wbm');
 
 app.use(cors());
@@ -9,21 +10,62 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get('/', function (req,res){
-  wbm.start().then(async wbmResponse => {
-    res.send(`<img src=${wbmResponse} alt="qrCode" />`);
-    await wbm.waitQRCode();
-    for (let index = 0; index < 4; index++) {
-      const phones = ['xxxxxx00001111'];
-      const message = `Loop ${index}`;
-      await wbm.send(phones, message);
+    res.send(`<p>Hello World!<p/>`);
+});
+
+app.post('/whatsgun', async function (req,res){
+  const companies = await axios.get('http://177.10.0.125:5001/v1/companies/all');
+  var { toSend } = req.body;
+  try {
+
+    wbm.start().then(async wbmResponse => {
+      res.send(`<img src=${wbmResponse} alt="qrCode" />`);
+      await wbm.waitQRCode();
+
+      for (var i = 0; i < companies.data.length; i++){
+        const responsePhone = (companies.data[i].phone).split('(51) 9').join('5551').replace(/\D/g, "");
+
+        const phones = [`${responsePhone}`];
+        const message = `${toSend}`;
+        await wbm.send(phones, message);
+      }
+
+      await wbm.end();
+    }).catch(err => console.log(err));
+
+  } catch(err) {
+    console.log(err);
+  }
+});
+
+app.post('/mailgun', function (req, res){
+  var { host, port, fromEmail, pass, toEmail, title, message, content } = req.body;
+    
+  let transporter = nodemailer.createTransport({
+    host: `${host}`,
+    port: `${port}`,
+    secure: true,
+    auth: {
+      user: `${fromEmail}`,
+      pass: `${pass}`
     }
-    await wbm.end();
-  }).catch(err => console.log(err));
+  });
+
+  transporter.sendMail({
+    from: `Nao responda <${fromEmail}>`,
+    to: `${toEmail}`,
+    subject: `${title}`,
+    text: `${message}`,
+    html: `${content}`
+  }).then(res => {
+    console.log(res);
+  }).catch(err => {
+    console.log(err);
+    res.sendStatus(400);
+  });
+  res.sendStatus(200);
 });
 
 app.listen(3000,() => {
   console.log('Server On');
 });
-
-
-
